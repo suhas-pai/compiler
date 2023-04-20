e9782d05b192a091228255bfa35147e928c78482
suhaspai <suhaspai64@gmail.com>
2023-04-20 00:31:30 -0400

    Add lowercased `kind.ts`

---
+++ ast/kind.ts
@@ -0,0 +1,9 @@
+enum ASTNodeKind {
+  GlobalScope = "global-scope",
+  IntegerLiteral = "integer-literal",
+  BinaryOperation = "binary-operation",
+  UnaryOperator = "unary-operator",
+  ParentExpression = "paren-expression",
+}
+
+export default ASTNodeKind;
